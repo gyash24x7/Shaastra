2eefe29e5ceea9c3ec9f31948e1cf438c60ca60b
Yash Gupta <gyash24x7@gmail.com>
2022-09-13 15:07:51 +0530

    added basic react app

--- libs/auth/src/auth.module.ts
+++ libs/auth/src/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { PassportModule } from "@nestjs/passport";
 import { AuthStrategy } from "./auth.strategy";
+import { ConfigModule } from "@nestjs/config";
 
 @Module( {
-	imports: [ PassportModule.register( { defaultStrategy: "jwt" } ) ],
+	imports: [ ConfigModule, PassportModule.register( { defaultStrategy: "jwt" } ) ],
 	providers: [ AuthStrategy ],
 	exports: [ PassportModule, AuthStrategy ]
 } )

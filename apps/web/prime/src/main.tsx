fec2b986b6b9d0f35e11948040497d1888be4249
Yash Gupta <gyash24x7@gmail.com>
2022-10-24 13:37:39 +0530

    added basic prime web app

---
+++ apps/web/prime/src/main.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import "@fontsource/fjalla-one/400.css";
+import "@fontsource/montserrat/300.css";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/700.css";
+
+ReactDOM.createRoot( document.getElementById( "root" ) as HTMLElement ).render(
+	<div style = { { fontFamily: "Montserrat", fontWeight: 500 } }>Hello Shaastra Prime!</div>
+);

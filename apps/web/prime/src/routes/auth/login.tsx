55d6cd51310cbf2d66593c6af5259a5aed3ef3c1
Yash Gupta <gyash24x7@gmail.com>
2022-12-30 22:36:24 +0530

    moved frontend app to solid start

--- apps/web/prime/src/routes/auth/login.tsx
+++ apps/web/prime/src/routes/auth/login.tsx
@@ -1,14 +1,13 @@
 import { Banner, Button, Form, minLengthValidator, patternValidator, TextInput, VStack } from "@shaastra/ui";
 import { exclamationTriangle, lockClosed, user } from "solid-heroicons/solid";
 import { Show } from "solid-js";
-import DarkLogo from "../../assets/DarkLogo.png";
 
 export const rollNumberRegex = /^[A-Z]{2}[0-9]{2}[A-Z][0-9]{3}$/;
 
 export default function LoginPage() {
 	return (
 		<VStack className = { "h-screen p-8" }>
-			<img src = { DarkLogo } alt = { "Shaastra Logo" } class = { "w-60 h-auto mx-auto my-4" }/>
+			<img src = { "/images/DarkLogo.png" } alt = { "Shaastra Logo" } class = { "w-60 h-auto mx-auto my-4" }/>
 			<h2 class = { "font-light text-3xl" }>LOGIN</h2>
 			<Form
 				initialValue = { { rollNumber: "", password: "" } }
